1e2ae36e222babaf7124eac0e1525deb118d8cde
CathLee <447932704@qq.com>
2024-01-06 21:27:59 +0800

    conf:Add Babel configuration and plugin for Vite

--- my-editor/vite.config.ts
+++ my-editor/vite.config.ts
@@ -5,9 +5,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path, { resolve } from 'path'
+import babel from 'vite-plugin-babel';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),babel()],
   // 其他配置项...
   resolve: {
     alias: {
