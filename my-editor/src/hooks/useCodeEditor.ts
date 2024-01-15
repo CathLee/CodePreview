f7be87b6e73e90d4f9517d556b51ad2332097cfb
CathLee <4447932704@qq.com>
2024-01-15 22:32:26 +0800

    fix:babel 解析jsx（仅js阶段

--- my-editor/src/hooks/useCodeEditor.ts
+++ my-editor/src/hooks/useCodeEditor.ts
@@ -6,6 +6,7 @@
 import { MutableRefObject, useCallback, useRef } from "react";
 import * as monaco from "monaco-editor";
 import { resolveImport } from "@/utils/babelHelper";
+import * as Babel from "@babel/core";
 
 export const useCodeEditor = (editorEl: MutableRefObject<HTMLDivElement|null>) => {
     let editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
@@ -42,14 +43,17 @@ export const useCodeEditor = (editorEl: MutableRefObject<HTMLDivElement|null>) =
             if (!editorRef.current) return reject("Editor not initialized");
             const code = editorRef.current!.getValue();
 
-            // 纯babel编译
-            // if (code === "") return;
-            // const _code = window.Babel.transform(code, {
-            //   presets: ["es2015", "react"],
-            // })?.code;
+            // babel解析react jsx
+            if (code === "") return;
+            const _code = window.Babel.transform(code, {
+              presets: ["env", "react"],
+            })?.code;
 
             // 对js环境进行编译
-            const _code = resolveImport(code, "importMap");
+            // 无论是js还是ts或者是其他框架语言都需要先编译成js
+            // const _code = resolveImport(code, "importMap");
+
+
             resolve(_code);
         });
     }, []);
